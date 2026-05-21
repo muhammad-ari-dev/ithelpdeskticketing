154c6df98f2c156c68cb382e0180e8a51fed9721
Muhammad Ari Ferdiansyah <muhammadariferdiansyah77@gmail.com>
2026-05-21 13:53:06 +0700

    Initial commit

---
+++ src/main/java/com/juaracoding/ITHelpdeskTicketing/ItHelpdeskTicketingApplication.java
@@ -0,0 +1,13 @@
+package com.juaracoding.ITHelpdeskTicketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItHelpdeskTicketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItHelpdeskTicketingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juaracoding/ITHelpdeskTicketing/ItHelpdeskTicketingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juaracoding.ITHelpdeskTicketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItHelpdeskTicketingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
